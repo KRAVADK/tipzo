ae436491f1524d241896e6d9c9e2cab8ed8b854b
Tipzo <TipZo@users.noreply.github.com>
2026-01-26 16:50:56 +0200

    Deploy tipzo_app_v7.aleo with update_profile registry fix

--- frontend/src/deployed_program.ts
+++ frontend/src/deployed_program.ts
@@ -1,4 +1,4 @@
-// Deployed program: tipzo_app_v6.aleo (build/main.leo)
+// Deployed program: tipzo_app_v7.aleo (build/main.leo)
 // Compatible with: profiles mapping, create_profile, update_profile, send_donation
 // Transitions: create_profile(name, bio), update_profile(name, bio), send_donation(recipient, amount, message, timestamp)
 // Mappings: 
@@ -6,5 +6,6 @@
 //   - active_profiles (u64 -> address) - public registry of all profiles
 //   - profile_count (u64 -> u64) - total number of registered profiles
 //   - is_registered (address -> bool) - track registered addresses
+// Version 7: Removed @noupgrade to allow future upgrades, update_profile now also registers profiles
 
-export const PROGRAM_ID = "tipzo_app_v6.aleo";
+export const PROGRAM_ID = "tipzo_app_v7.aleo";
